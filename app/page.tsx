3ab18dd6128c6d6e0f1ba5fc6f51918be9064ff1
KeddyB <bokelusi@gmail.com>
2024-02-22 21:43:48 +0100

    this is the final commit of the day

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
     <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <Navbar />
       <Live />
     </div>    
   );
