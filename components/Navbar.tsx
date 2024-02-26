0fa4d9b2a8a714e459bc050c0bd30b0e633d4569
KeddyB <bokelusi@gmail.com>
2024-02-26 12:23:09 +0100

    this commit contains some errors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,23 +4,23 @@ import Image from "next/image";
 import { memo } from "react";
 
 import { navElements } from "@/constants";
-import { ActiveElement, NavbarProps } from "@/types/type";
+import { ActiveElement, NavbarProps } from "@/assets/types/type";
 
-import { Button } from "./ui/button";
-import ShapesMenu from "./ShapesMenu";
+// import { Button } from "./ui/button";
+// import ShapesMenu from "./ShapesMenu";
 import ActiveUsers from "./users/ActiveUsers";
-import { NewThread } from "./comments/NewThread";
+// import { NewThread } from "./comments/NewThread";
 
 const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveElement }: NavbarProps) => {
-  const isActive = (value: string | Array<ActiveElement>) =>
-    (activeElement && activeElement.value === value) ||
-    (Array.isArray(value) && value.some((val) => val?.value === activeElement?.value));
+  // const isActive = (value: string | Array<ActiveElement>) =>
+  //   (activeElement && activeElement.value === value) ||
+  //   (Array.isArray(value) && value.some((val) => val?.value === activeElement?.value));
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
 
-      <ul className="flex flex-row">
+      {/*<ul className="flex flex-row">
         {navElements.map((item: ActiveElement | any) => (
           <li
             key={item.name}
@@ -33,7 +33,7 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
             `}
           >
             {/* If value is an array means it's a nav element with sub options i.e., dropdown */}
-            {Array.isArray(item.value) ? (
+            {/*{Array.isArray(item.value) ? (
               <ShapesMenu
                 item={item}
                 activeElement={activeElement}
@@ -65,7 +65,7 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
             )}
           </li>
         ))}
-      </ul>
+      </ul> */}
 
       <ActiveUsers />
     </nav>
